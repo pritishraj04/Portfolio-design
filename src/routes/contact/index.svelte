5f21cd3355111badd2d69597955091028bfcbb65
pritishraj04 <pritishraj04@gmail.com>
2022-02-28 17:22:49 +0530

    netlify form bug fixes

--- src/routes/contact/index.svelte
+++ src/routes/contact/index.svelte
@@ -3,7 +3,14 @@
   action="/contact/success"
   method="POST"
   data-netlify="true"
+  data-netlify-honeypot="bot-field"
 >
+  <input type="hidden" name="form-name" value="contact" />
+  <p hidden>
+    <label>
+      Don't fill this out: <input name="bot-field" />
+    </label>
+  </p>
   <div class="form-group">
     <label for="name"> Name </label>
     <input
