13e71a53926a695f8105601375532a484136bc87
pritishraj04 <pritishraj04@gmail.com>
2022-02-28 15:43:09 +0530

    work page filter added

--- src/routes/work.svelte
+++ src/routes/work.svelte
@@ -1,35 +1,123 @@
 <script>
   import works from "$lib/works";
+  let filter = { techFilter: [], categoryFilter: [] };
+  let filteredList = works;
+
+  const refreshList = (type) => {
+    if (filter.techFilter.length === 0 && filter.categoryFilter.length === 0) {
+      filteredList = works;
+    } else {
+      if (type === "category") {
+        for (let i = 0; i < filter.categoryFilter.length; i++) {
+          filteredList = works.filter((e) =>
+            e.category.includes(filter.categoryFilter[i])
+          );
+        }
+        filteredList = filteredList;
+      } else {
+        for (let i = 0; i < filter.techFilter.length; i++) {
+          filteredList = works.filter((e) =>
+            e.tech.includes(filter.techFilter[i])
+          );
+        }
+        filteredList = filteredList;
+      }
+    }
+  };
+
+  const addFilter = (type, fil) => {
+    switch (type) {
+      case "category":
+        if (!filter.categoryFilter.includes(fil)) {
+          filter.categoryFilter = [...filter.categoryFilter, fil];
+          refreshList("category");
+        }
+        break;
+      case "tech":
+        if (!filter.techFilter.includes(fil)) {
+          filter.techFilter = [...filter.techFilter, fil];
+          refreshList("tech");
+        }
+        break;
+    }
+  };
+  const removeFilter = (type, fil) => {
+    switch (type) {
+      case "category":
+        for (let i = 0; i < filter.categoryFilter.length; i++) {
+          if (filter.categoryFilter[i] === fil) {
+            filter.categoryFilter.splice(i, 1);
+            refreshList("category");
+          }
+        }
+        break;
+      case "tech":
+        for (let i = 0; i < filter.techFilter.length; i++) {
+          if (filter.techFilter[i] === fil) {
+            filter.techFilter.splice(i, 1);
+            refreshList("tech");
+          }
+        }
+        break;
+    }
+    filter = filter;
+  };
 </script>
 
 <main>
   <h1>Things I've worked on</h1>
+  <h3>
+    Filter by: {#if filter.techFilter.length === 0 && filter.categoryFilter.length === 0}
+      All
+    {:else}
+      {#each filter.techFilter as item}
+        <button class="tech" on:click={() => removeFilter("tech", item)}
+          >{item}</button
+        >
+      {/each}
+      {#each filter.categoryFilter as item}
+        <button class="category" on:click={() => removeFilter("category", item)}
+          >{item}</button
+        >
+      {/each}
+    {/if}
+  </h3>
   <section class="works">
-    {#each works as work}
-      <a
-        href={work.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        class="work-card"
-      >
+    {#each filteredList as work}
+      <div class="work-card">
         <div class="techs">
           {#each work.tech as tech}
-            <a href="/" class="tech" title={`Filter by ${tech}`}
-              ><h5>{tech}</h5></a
+            <button
+              class="tech"
+              on:click={() => addFilter("tech", tech)}
+              title={`Filter by ${tech}`}>{tech}</button
             >
           {/each}
         </div>
-        <img src={`/asset/works/${work.img}`} alt={work.title} />
-        <h4>{work.title}</h4>
-        <a href="/" class="category" title={`Filter by ${work.category}`}
-          ><h5>{work.category}</h5></a
+        <a href={work.link} target="_blank" rel="noopener noreferrer">
+          <img src={`/asset/works/${work.img}`} alt={work.title} /></a
         >
-        <p>
-          {work.desc}
-        </p>
-      </a>
+        <div class="desc">
+          <a href={work.link} target="_blank" rel="noopener noreferrer">
+            <h4>{work.title}</h4></a
+          >
+          <button
+            on:click={() => addFilter("category", work.category)}
+            class="category"
+            title={`Filter by ${work.category}`}>{work.category}</button
+          >
+          <a href={work.link} target="_blank" rel="noopener noreferrer">
+            <p>
+              {work.desc}
+            </p></a
+          >
+        </div>
+      </div>
     {:else}
-      <h2>Someting went wrong. Please visit <a href="/">Home</a></h2>
+      <h2>
+        No items found. Try removing filters or please visit <a href="/">Home</a
+        >
+      </h2>
     {/each}
   </section>
 </main>
@@ -41,6 +129,26 @@
       font-weight: 400;
       font-size: clamp(1.6rem, 2.2vw, 3.125rem);
       margin-bottom: 2rem;
+    }
+    h3 {
+      font-size: clamp(1rem, 1.6vw, 2.725rem);
+      font-weight: 600;
+      margin-bottom: 1rem;
+      button.tech {
+        margin: 0.2rem;
+        padding: 0.2rem;
+        border-radius: 0.5rem;
+        font-weight: 600;
+      }
+      button.category {
+        margin: 0.2rem;
+        padding: 0.2rem;
+        border-radius: 0.5rem;
+        font-weight: 600;
+        background: var(--clr-primary-bg);
+        border: 2px var(--clr-primary-text) solid;
+        text-transform: capitalize;
+      }
     }
     .works {
       display: grid;
@@ -56,6 +164,7 @@
       .work-card {
         min-width: 6.25rem;
         transition: transform 0.3s ease;
+        border: 1px var(--clr-secondary-bg) solid;
         &:hover {
           background: var(--clr-primary-bg);
           transform: scale(1.1);
@@ -63,27 +172,38 @@
         img {
           width: 100%;
         }
-        h4 {
-          font-weight: 400;
-          font-size: clamp(1rem, 1.6vw, 2.725rem);
-          margin-bottom: 0.2rem;
-        }
-        .category,
-        .tech {
-          margin-block: 0.4rem;
-          display: block;
-          h5 {
+        .desc {
+          padding: 0.5rem;
+          h4 {
+            font-weight: 400;
+            font-size: clamp(1rem, 1.6vw, 2.725rem);
+            margin-bottom: 0.2rem;
+          }
+          .category {
+            margin-block: 0.4rem;
+            display: block;
+            background: var(--clr-primary-bg);
             display: inline-block;
             text-transform: capitalize;
             font-size: clamp(0.7rem, 0.7vw, 1rem);
+            font-weight: 600;
             border: 2px var(--clr-primary-text) solid;
             border-radius: 0.5rem;
             padding: 0.2rem;
+          }
+          p {
+            font-size: clamp(0.8rem, 0.9vw, 1.925rem);
+            line-height: 1.2;
+            opacity: 0.7;
           }
         }
         .techs {
           position: absolute;
           max-height: 60%;
+          @media only screen and (max-width: 768px) {
+            position: unset;
+            display: flex;
+          }
           &::before {
             content: "⚙️";
             background: rgba(221, 221, 221, 0.7);
@@ -98,23 +218,19 @@
           }
           .tech {
             transition: transform 0.3s ease;
-            margin-block: 0.2rem;
+            display: block;
+            margin: 0.2rem;
+            padding: 0.2rem;
+            border-radius: 0.5rem;
+            font-size: clamp(0.6rem, 0.6vw, 0.9rem);
             transform: scale(0);
             display: none;
+            font-weight: 600;
             @media only screen and (max-width: 768px) {
               transform: scale(1);
               display: block;
             }
-            h5 {
-              font-size: clamp(0.6rem, 0.6vw, 0.9rem);
-              background: var(--clr-primary-bg);
-            }
           }
-        }
-        p {
-          font-size: clamp(0.8rem, 0.9vw, 1.925rem);
-          line-height: 1.2;
-          opacity: 0.7;
         }
       }
     }
